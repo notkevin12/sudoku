547c1346b3b2886f69c00981e6bf28accae46901
Kevin Lai <clai@ucsb.edu>
2023-12-23 15:29:50 -0800

    Cell, row, and col check methods

--- sdku.c
+++ sdku.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #define digit_t unsigned char
 #define DIGITS 9
+#define CELL_DIM 3
 
 void printgrid(digit_t* grid)
 {
@@ -13,6 +15,52 @@ void printgrid(digit_t* grid)
         }
         printf("\n");
     }
+}
+
+/* T if the cell is valid, F otherwise */
+int checkcell(digit_t* grid, int r, int c)
+{
+    int block_r = r / CELL_DIM * CELL_DIM;
+    int block_c = c / CELL_DIM * CELL_DIM;
+    static int seen[DIGITS];
+    memset(seen, DIGITS, 0);
+    for (int i = block_r; i < block_r + CELL_DIM; ++i) {
+        for (int j = block_c; j < block_c + CELL_DIM; ++j) {
+            digit_t d = grid[i * DIGITS + j] - 1;
+            if (seen[d])
+                return 0;
+            seen[d] = 1;
+        }
+    }
+    return 1;
+}
+
+/* T if the row is valid, F otherwise */
+int checkrow(digit_t* grid, int r)
+{
+    static int seen[DIGITS];
+    memset(seen, DIGITS, 0);
+    for (int c = 0; c < DIGITS; ++c) {
+        digit_t d = grid[r * DIGITS + c] - 1;
+        if (seen[d])
+            return 0;
+        seen[d] = 1;
+    }
+    return 1;
+}
+
+/* T if the column is valid, F otherwise */
+int checkcol(digit_t* grid, int c)
+{
+    static int seen[DIGITS];
+    memset(seen, DIGITS, 0);
+    for (int r = 0; r < DIGITS; ++r) {
+        digit_t d = grid[r * DIGITS + c] - 1;
+        if (seen[d])
+            return 0;
+        seen[d] = 1;
+    }
+    return 1;
 }
 
 int main()
@@ -25,5 +73,6 @@ int main()
         }
     }
     printgrid(grid);
+    printf("%s\n", (checkcell(grid, 1, 1) ? "true" : "false"));
     free((void*)grid);
 }
